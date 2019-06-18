0996e39cec4141711d29fef568609c8178605b5a
Jimmie Johansson <jimmitjoo@gmail.com>
2019-06-18 21:54:17 +0200

    Restart on the engine, the old one is really messy

--- app/Engines/MatchEngine.php
+++ app/Engines/MatchEngine.php
@@ -9,7 +9,24 @@
 namespace App\Engines;
 
 
+use App\Lineup;
+use App\TournamentGame;
+
 class MatchEngine
 {
+    private $game;
+    private $hometeamLineup;
+    private $awayteamLineup;
 
+    public function __construct(TournamentGame $game)
+    {
+        $this->game = $game;
+        $this->hometeamLineup = Lineup::where('club_id', $this->game->hometeam_id)->where('team', $this->game->hometeam_squad)->first();
+        $this->awayteamLineup = Lineup::where('club_id', $this->game->awayteam_id)->where('team', $this->game->awayteam_squad)->first();
+    }
+
+    private function getLineups()
+    {
+
+    }
 }
