f71c384bffb2df169ab3db460b69f06ca48b350c
Jimmie Johansson <jimmitjoo@gmail.com>
2019-06-18 21:53:45 +0200

    Create Seasons

---
+++ database/migrations/2019_06_18_175325_create_seasons_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSeasonsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('seasons', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->dateTime('start_time');
+            $table->dateTime('end_time');
+            $table->timestamps();
+        });
+
+        // Insert a season by default
+        DB::table('seasons')->insert([
+            'start_time' => date('Y-m-d'),
+            'end_time' => date('Y-m-d 23:59:59', strtotime('+3 months')),
+            'created_at' => date('Y-m-d H:i:s'),
+            'updated_at' => date('Y-m-d H:i:s'),
+        ]);
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('seasons');
+    }
+}
