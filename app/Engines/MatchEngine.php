3528b597848495aca4e706929e97222d7d623bdb
Jimmie Johansson <jimmie@codebet.se>
2019-06-24 09:18:51 +0200

    WIP winning chances

--- app/Engines/MatchEngine.php
+++ app/Engines/MatchEngine.php
@@ -10,6 +10,7 @@ namespace App\Engines;
 
 
 use App\Lineup;
+use App\Person;
 use App\TournamentGame;
 
 class MatchEngine
@@ -18,15 +19,28 @@ class MatchEngine
     private $hometeamLineup;
     private $awayteamLineup;
 
+    protected $hometeamPositions;
+    protected $awayteamPositions;
+
     public function __construct(TournamentGame $game)
     {
         $this->game = $game;
         $this->hometeamLineup = Lineup::where('club_id', $this->game->hometeam_id)->where('team', $this->game->hometeam_squad)->first();
         $this->awayteamLineup = Lineup::where('club_id', $this->game->awayteam_id)->where('team', $this->game->awayteam_squad)->first();
+
+        $this->winningChances();
+
+        dd([
+            $this->hometeamLineup,
+            $this->awayteamLineup,
+        ]);
     }
 
-    private function getLineups()
+    private function winningChances()
     {
-
+        for ($i=1;$i<12;$i++) {
+            $this->hometeamLineup->{'player_' . $i} = Person::find($this->hometeamLineup->{'player_' . $i});
+            $this->awayteamLineup->{'player_' . $i} = Person::find($this->awayteamLineup->{'player_' . $i});
+        }
     }
 }
