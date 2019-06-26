ca8935e46a5f55cf99b72b5f82688dd8852b5bd7
Jimmie Johansson <jimmitjoo@gmail.com>
2019-06-26 20:16:59 +0200

    Cast string to int

--- app/TournamentStanding.php
+++ app/TournamentStanding.php
@@ -8,6 +8,8 @@ class TournamentStanding extends Model
 {
     protected $guarded = [];
 
+    protected $casts = ['group_id' => 'int'];
+
     public function club()
     {
         return $this->belongsTo(Club::class);
