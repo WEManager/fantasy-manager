88d9407d7afe249d93cd037a3aefafec4ce6c46c
Jimmie Johansson <jimmitjoo@gmail.com>
2019-06-26 19:52:20 +0200

    Fix bug regarding language

--- bin/init.sh
+++ bin/init.sh
@@ -1,22 +1,12 @@
 #!/bin/bash
 
+echo Installing dependencies
+composer install
+
+npm install
+
 echo Generating new key
 php artisan key:generate
 
 echo Refreshing the database
-php artisan migrate:refresh
-
-echo Creating 16 swedish clubs
-php artisan club:generate 16
-
-echo Creating 16 spanish clubs
-php artisan club:generate 16 es
-
-echo Creating 16 german clubs
-php artisan club:generate 16 de
-
-echo Creating 16 british clubs
-php artisan club:generate 16 gb
-
-echo Creating 16 italian clubs
-php artisan club:generate 16 it
+php artisan migrate
