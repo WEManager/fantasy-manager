fd2980b2475f2a709bd642443c10aa005436256c
Jimmie Johansson <jimmitjoo@gmail.com>
2019-06-27 23:17:59 +0200

    Add material icons

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -38,7 +38,9 @@
                     <li class="nav-item dropdown">
                         <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
                            data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                            {{ __('Choose language') }} <span class="caret"></span>
+                            <i class="material-icons">
+                                translate
+                            </i> <span class="caret"></span>
                         </a>
 
                         <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
