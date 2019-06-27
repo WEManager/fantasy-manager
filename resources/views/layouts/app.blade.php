a6ffc9edac6f23edc7b5547e7cf51eed8c820052
Jimmie Johansson <jimmie@codebet.se>
2019-06-27 14:20:17 +0200

    SEO title

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>@yield('title') {{ config('app.name', 'Laravel') }} {{ __('Football Manager Game') }}</title>
+    <title>@yield('title') {{ __('Football Manager Game') }} - {{ config('app.name', 'Laravel') }}</title>
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
