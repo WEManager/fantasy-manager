74ebe39fdb67854263a16a56278b0886b8f4c428
Marcelo Oliveira <marcelotk_15@hotmail.com>
2022-08-30 15:30:53 -0300

    📦 NEW: nation Model and player relation

---
+++ database/migrations/2022_08_30_113427_create_nations_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('nations', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->integer('fifa_id')->unique();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('nations');
+    }
+};
---
+++ database/migrations/2022_08_30_113552_relationship_player_nation.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('players', function (Blueprint $table) {
+            $table->integer('nation_fifa_id')->nullable();
+
+            $table->foreign('nation_fifa_id')->references('fifa_id')->on('nations');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('players', function (Blueprint $table) {
+            $table->dropForeign('players_nation_fifa_id_foreign');
+            $table->dropColumn(['nation_fifa_id']);
+        });
+    }
+};
