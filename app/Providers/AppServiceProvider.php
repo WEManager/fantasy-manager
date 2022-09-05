265ffdc278af5307dd337dd24094304a53fd5ad9
Marcelo Oliveira <marcelotk_15@hotmail.com>
2022-09-05 13:28:49 -0300

    🐛 FIX:

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -15,7 +15,7 @@ class AppServiceProvider extends ServiceProvider {
     }
 
     public function boot(): void {
-        // URL::forceScheme('https');
+        URL::forceScheme('https');
 
         Player::observe(PlayerObserver::class);
 
