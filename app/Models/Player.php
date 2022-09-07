616197361bff2f7e0ed364e46fd517dee1167559
Marcelo Oliveira <marcelotk_15@hotmail.com>
2022-09-07 00:04:13 -0300

    👌 IMPROVE: idented

--- app/Models/Player.php
+++ app/Models/Player.php
@@ -9,31 +9,26 @@ use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Database\Eloquent\Relations\HasOneThrough;
 
 class Player extends Model {
-    use PlayerTraitStats;
-    use PlayerOveralls;
+  use PlayerTraitStats;
+  use PlayerOveralls;
 
-    protected $guarded = [];
+  protected $guarded = [];
 
-    /**
-     * The relationships that should always be loaded.
-     *
-     * @var array
-     */
-    protected $with = ['nation'];
+  protected $with = ['nation'];
 
-    public function nation(): HasOne {
-        return $this->hasOne(Nation::class, 'fifa_id', 'nation_fifa_id');
-    }
+  public function nation(): HasOne {
+    return $this->hasOne(Nation::class, 'fifa_id', 'nation_fifa_id');
+  }
 
-    public function club(): HasOneThrough {
-        return $this->hasOneThrough(Club::class, Contract::class, 'player_id', 'id', 'id', 'club_id')
-            ->whereDate('from', '<', date('Y-m-d'))
-            ->whereDate('until', '>', date('Y-m-d'));
-    }
+  public function club(): HasOneThrough {
+    return $this->hasOneThrough(Club::class, Contract::class, 'player_id', 'id', 'id', 'club_id')
+      ->whereDate('from', '<', date('Y-m-d'))
+      ->whereDate('until', '>', date('Y-m-d'));
+  }
 
-    public function contract() {
-        return $this->hasOne(Contract::class, 'player_id')
-            ->whereDate('from', '<', date('Y-m-d'))
-            ->whereDate('until', '>', date('Y-m-d'));
-    }
+  public function contract() {
+    return $this->hasOne(Contract::class, 'player_id')
+      ->whereDate('from', '<', date('Y-m-d'))
+      ->whereDate('until', '>', date('Y-m-d'));
+  }
 }
