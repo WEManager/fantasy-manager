50a7d0f4688729fd680999aa9d28e87602559feb
Marcelo 	eka Oliveira <marcelotk_15@hotmail.com>
2025-08-22 11:17:10 -0300

    chore(delete): remove dist path

--- resources/js/components/dist/InputError.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-exports.__esModule = true;
-function InputError(_a) {
-    var message = _a.message, _b = _a.className, className = _b === void 0 ? '' : _b;
-    if (message)
-        return React.createElement("p", { className: 'text-sm text-red-600 ' + className }, message);
-    return React.createElement(React.Fragment, null);
-}
-exports["default"] = InputError;
--- resources/js/components/dist/Label.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-exports.__esModule = true;
-function Label(_a) {
-    var forInput = _a.forInput, value = _a.value, className = _a.className, children = _a.children;
-    return (React.createElement("label", { htmlFor: forInput, className: "block font-medium text-sm text-gray-700 " + className }, value ? value : children));
-}
-exports["default"] = Label;
