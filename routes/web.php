38ccaa1bb66d653ec4734e4585265cf59e724038
Marcelo 	eka Oliveira <marcelotk_15@hotmail.com>
2025-08-26 16:35:47 -0300

    chore(routes): clean up unused controllers and adjust namespaces
    - Removed PageController and its unused dynamic slug route
    - Updated Livewire default namespace to `App\Http\Livewire`
    - Simplified and reformatted `web.php` routes

--- routes/web.php
+++ routes/web.php
@@ -51,44 +51,23 @@ Route::get('partidas/{game}', Controllers\TournamentGameController::class)
   ->name('game.show');
 
 Route::get('/dashboard', function () {
-    return Inertia::render('Dashboard');
+  return Inertia::render('Dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
-// Route::get('/{slug}', [PageController::class, 'show'])->name('page');
-
-// Route::get('/players/create', 'PlayerController@create')->middleware('admin')->name('create_player');
-// Route::put('/players/{user}', 'PlayerController@update')->middleware('admin')->name('update_player');
-// Route::get('/players/{user}/edit', 'PlayerController@edit')->middleware('admin')->name('edit_player');
-// Route::post('/players', 'PlayerController@store')->middleware('admin')->name('store_player');
-// Route::get('/players/{person}', 'PlayerController@show')->name('show_player');
-// Route::get('/players', 'PlayerController@index')->name('list_players');
-
-// Route::get('/tournaments/create', 'TournamentController@create')->middleware('admin')->name('create_tournament');
-// Route::post('/tournaments', 'TournamentController@store')->middleware('auth')->name('store_tournament');
-// Route::get('/tournaments/list', 'TournamentController@index')->name('list_tournaments');
-
-// Route::get('/clubs/list', 'ClubController@index')->name('list_clubs');
-// Route::get('{club}', 'ClubController@show')->name('show_club');
-
-Route::post('{club}', 'ClubController@store')->middleware('admin')->name('store_club');
-// Route::get('{club}/edit', 'ClubController@edit')->middleware('admin')->name('edit_club');
 Route::get('{club}/{squad}/lineup', 'LineupController@edit')->name('edit_lineup');
-
 Route::post('/update-lineup/{lineup}', 'LineupController@update')->name('update_lineup');
 
-Route::get('/games/{game}', 'TournamentGameController@show')->name('show_game');
-
 Route::get('/test-heading', function () {
-    $player = \App\Models\Person::find(1);
-    $duelant = \App\Models\Person::find(2);
+  $player = \App\Models\Person::find(1);
+  $duelant = \App\Models\Person::find(2);
 
-    echo \App\Engines\GameDuelsEngine::headingDuelBetween($player, $duelant, true);
+  echo \App\Engines\GameDuelsEngine::headingDuelBetween($player, $duelant, true);
 });
 
 Route::get('/test-game/{id}', function ($id) {
-    $game = \App\Models\TournamentGame::find($id);
+  $game = \App\Models\TournamentGame::find($id);
 
-    new \App\Engines\MatchEngine($game);
+  new \App\Engines\MatchEngine($game);
 });
 
 // JUST TO NOT BREAK THE APP
