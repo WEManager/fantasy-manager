33d4a04b3e7f2152481349698895fccadf48c1ef
Marcelo 	eka Oliveira <marcelotk_15@hotmail.com>
2025-08-26 22:37:27 -0300

    refactor(clubs): remove old Livewire club editing views and components
    - Deleted `EditClub` Livewire component and related Blade views
    - Removed outdated Blade templates for club edit, job list, header, sidemenu, and partials
    - Consolidated club editing into Inertia + React flow

--- resources/views/clubs/partials/clubname.blade.php
+++
@@ -1,2 +0,0 @@
-<a href="{{ link_route('club.show', ['club' => $club]) }}">{{ $club->name }}</a>
-<div class="club_colors" style="background-color: {{ $club->colors[0] }}; border-color: {{ $club->colors[1] }}"></div>
